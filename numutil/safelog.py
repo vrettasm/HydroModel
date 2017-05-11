38f3f4efbb8581c1355a9b0e79158472fe32e610
vrettasm <vrettasm@gmail.com>
2017-05-11 19:55:29 +0100

    Added safelog file

--- numutil/safelog.py
+++ numutil/safelog.py
@@ -4,8 +4,9 @@ from numpy import log, array, asarray
 # This module includes:
 __all__ = ['safelog']
 
+
 # SAFELOG:
-def safelog(x = None):
+def safelog(x=None):
     ''' Safe logarithm
     
     Description:
@@ -38,35 +39,35 @@ def safelog(x = None):
     References:
         (N/A)
     '''
-    
+
     # Prevent empty input.
-    if(x == None):
+    if (x==None):
         print(" [safelog] in debug mode ... exiting:", end="")
         return None
-    
+
     # Define LOWER and UPPER bounds.
     _LWR_Bound = 1.0E-300
     _UPR_Bound = 1.0E+300
-    
+
     # Make sure input is an array.
     x = asarray(x)
-    
+
     # Overflow Error can happen if the input is "int64"
     try:
         # Check for scalar.
         if (x.ndim == 0):
-            if (x <_LWR_Bound):
-                x =_LWR_Bound
-            elif (x >_UPR_Bound):
-                x =_UPR_Bound
-            #_end_if
+            if (x < _LWR_Bound):
+                x = _LWR_Bound
+            elif (x > _UPR_Bound):
+                x = _UPR_Bound
+                # _end_if
         else:
             # Check Lower/Upper bounds
-            x[x <_LWR_Bound] =_LWR_Bound
-            x[x >_UPR_Bound] =_UPR_Bound
+            x[x < _LWR_Bound] = _LWR_Bound
+            x[x > _UPR_Bound] = _UPR_Bound
     except OverflowError:
-        return safelog(array(x, dtype = 'float'))
-    
+        return safelog(array(x, dtype='float'))
+
     # Return the log of the filtered input.
     return log(x)
-# == END OF FILE ==
+    # == END OF FILE ==
