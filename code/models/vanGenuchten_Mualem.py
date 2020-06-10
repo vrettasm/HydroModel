04c86937c18b42907031c461f780f62f6a339820
vrettasm <vrettasm@gmail.com>
2020-06-10 19:25:57 +0200

    Initial commit of new hydrologic models.

---
+++ code/models/vanGenuchten_Mualem.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class vanGenuchtenMualem(object):
+    """
+        This class represents a hydrologic model based on the vanGenuchten-Mualem model.
+
+        -- REFERENCE --
+        M. Th. van Genuchten, 1980: A closed-form equation for predicting the hydraulic conductivity
+        of unsaturated soils. Soil Science Society of America Journal 44, pp: 892-898.
+        -- REFERENCE --
+
+    """
+    pass
+
+# _end_class_
---
+++ code/src/soil_properties.py
@@ -0,0 +1,7 @@
+class SoilProperties(object):
+    """
+        This class represents Soil properties.
+    """
+    pass
+
+# _end_class_
