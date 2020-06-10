41a5613d2636cda163f701ccdc315d000e42d3b8
vrettasm <vrettasm@gmail.com>
2020-06-10 19:28:48 +0200

    Added documentation.

--- code/models/vanGenuchten.py
+++ code/models/vanGenuchten.py
@@ -5,7 +5,7 @@ class vanGenuchten(object):
         This class represents a hydrologic model based on the vanGenuchten model.
 
         -- REFERENCE --
-        M. Th. van Genuchten, 1980: A closed-form equation for predicting the hydraulic conductivity
+        M. Th. van Genuchten (1980), A closed-form equation for predicting the hydraulic conductivity
         of unsaturated soils. Soil Science Society of America Journal 44, pp: 892-898.
         -- REFERENCE --
 
