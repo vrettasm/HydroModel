00394261a9d17f6d05a5ba48a50b87ad6724b938
vrettasm <vrettasm@gmail.com>
2020-06-12 12:09:56 +0200

    Update the interface.

--- code/src/soil_properties.py
+++ code/src/soil_properties.py
@@ -5,16 +5,16 @@ class SoilProperties(object):
         This class represents Soil properties.
     """
 
-    __slots__ = ("n", "a0", "psi_sat", "epsilon")
+    __slots__ = ("param_n", "param_a", "param_psi_sat", "param_epsilon")
 
     # Default constructor.
-    def __init__(self, n: float = 2.0, a0: float = 0.009, psi_sat: float = -100.0,
+    def __init__(self, n: float = 2.0, alpha: float = 0.009, psi_sat: float = -100.0,
                  epsilon: float = 1.0e-7):
         """
         Soil properties default constructor.
 
         :param n: (float) is a measure of the pore-size distribution [-]
-        :param a0: (float) is related to the inverse of the air entry suction [1/L : 1/cm]
+        :param alpha: (float) is related to the inverse of the air entry suction [1/L : 1/cm]
         :param psi_sat: (float) suction at saturation [L: cm]
         :param epsilon: (float) tiny positive threshold value [-]
         """
@@ -22,37 +22,37 @@ class SoilProperties(object):
         # Check for allowed range.
         if n > 1.0:
             # Pore size distribution parameter.
-            self.n = n
+            self.param_n = n
         else:
             raise ValueError(" Soil property 'n' should be > 1.")
         # _end_if_
 
         # Check for allowed range.
-        if a0 > 0.0:
-            self.a0 = a0
+        if alpha > 0.0:
+            self.param_a = alpha
         else:
-            raise ValueError(" Soil property 'a0' should be strictly positive.")
+            raise ValueError(" Soil property 'alpha' should be strictly positive.")
         # _end_if_
 
-        # Pressure head (suction) should be only negative.
+        # Pressure head (suction) should only be negative.
         # If zero value is given it means full saturation.
-        self.psi_sat = np.minimum(psi_sat, 0.0)
+        self.param_psi_sat = np.minimum(psi_sat, 0.0)
 
         # This should be a small positive number.
-        self.epsilon = np.maximum(epsilon, 1.0e-8)
+        self.param_epsilon = np.maximum(epsilon, 1.0e-8)
     # _end_def_
 
     @property
-    def param_n(self):
-        return self.n
+    def n(self):
+        return self.param_n
     # _end_def_
 
-    @param_n.setter
-    def param_n(self, new_value):
+    @n.setter
+    def n(self, new_value):
         # Accept only positive values.
         if new_value > 1.0:
             # Make the change.
-            self.n = new_value
+            self.param_n = new_value
         else:
             # Raise an error with a message.
             raise ValueError(" Soil property 'n': {},"
@@ -61,39 +61,39 @@ class SoilProperties(object):
     # _end_def_
 
     @property
-    def param_m(self):
-        return 1.0 - (1.0/self.n)
+    def m(self):
+        return 1.0 - (1.0/self.param_n)
     # _end_def_
 
     @property
-    def param_a0(self):
-        return self.a0
+    def alpha(self):
+        return self.param_a
     # _end_def_
 
-    @param_a0.setter
-    def param_a0(self, new_value):
+    @alpha.setter
+    def alpha(self, new_value):
         # Accept only positive values.
         if new_value > 0.0:
             # Make the change.
-            self.a0 = new_value
+            self.param_a = new_value
         else:
             # Raise an error with a message.
-            raise ValueError(" Soil property 'a0': {},"
+            raise ValueError(" Soil property 'alpha': {},"
                              " should be strictly positive.".format(new_value))
         # _end_if_
     # _end_def_
 
     @property
-    def psiSat(self):
-        return self.psi_sat
+    def psi_sat(self):
+        return self.param_psi_sat
     # _end_def_
 
-    @psiSat.setter
-    def psiSat(self, new_value):
+    @psi_sat.setter
+    def psi_sat(self, new_value):
         # Accept only negative values.
         if new_value <= 0.0:
             # Make the change.
-            self.psi_sat = new_value
+            self.param_psi_sat = new_value
         else:
             # Raise an error with a message.
             raise ValueError(" Soil property 'psi_sat': {},"
@@ -102,16 +102,16 @@ class SoilProperties(object):
     # _end_def_
 
     @property
-    def param_epsilon(self):
-        return self.epsilon
+    def epsilon(self):
+        return self.param_epsilon
     # _end_def_
 
-    @param_epsilon.setter
-    def param_epsilon(self, new_value):
+    @epsilon.setter
+    def epsilon(self, new_value):
         # Accept only negative values.
         if new_value > 0.0:
             # Make the change.
-            self.epsilon = new_value
+            self.param_epsilon = new_value
         else:
             # Raise an error with a message.
             raise ValueError(" Soil property 'epsilon': {},"
