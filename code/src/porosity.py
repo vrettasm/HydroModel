9eefecfd9f212533e2582f16b664df8275ec58ce
vrettasm <vrettasm@gmail.com>
2020-06-12 16:17:35 +0200

    Update Porosity class.

--- code/src/porosity.py
+++ code/src/porosity.py
@@ -1,3 +1,28 @@
+# Imports...
+
 class Porosity(object):
-    pass
+    """
+
+    """
+
+    __slots__ = ("profile", "field_cap", "wilting_point")
+
+    def __init__(self):
+        pass
+    # _end_def_
+
+    def __call__(self, z):
+        """
+        TBD
+
+        :param z:
+        :return: the porosity profile at depth(s) 'z'.
+        """
+        return z
+    # _end_def_
+
+    def __str__(self):
+        pass
+    # _end_def_
+
 # _end_class_
