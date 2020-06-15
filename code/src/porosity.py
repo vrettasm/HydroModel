0176e445b56fd76b22c30f25cb8e56d1396dde21
vrettasm <vrettasm@gmail.com>
2020-06-15 18:05:05 +0200

    Added documentation and finalized the class.

--- code/src/porosity.py
+++ code/src/porosity.py
@@ -1,16 +1,39 @@
 import numpy as np
-
+from scipy.interpolate import interp1d
 
 class Porosity(object):
     """
     This class creates a porosity object. A porosity profile (field) is generated,
     as a function of the depth (z), along with the relevant field capacity and
     wilting point profiles (as functions of depth).
+
+    Naturally the porosity in the soil decreases with depth because the free empty
+    pores are smaller in size, therefore allowing for less water to be kept at any
+    given depth. However, depending on the type (or mixture) of soils this profile
+    can vary.
     """
 
-    __slots__ = ("profile", "field_cap", "wilting_point", "sap_layer_idx", "web_Layer_idx")
+    __slots__ = ("profile", "field_cap", "wilting_point", "f_interp", "p_model")
 
     def __init__(self, z_grid, layers, theta, soil, p_model):
+        """
+        Constructs an object that will hold the porosity, field capacity and wilting point
+        profiles, for the z-domain (increasing downwards).
+
+        :param z_grid: depth values at which we want to generate the porosity values.
+        :param layers: tuple with the underground layers.
+        :param theta: water content object.
+        :param soil: soil properties object.
+        :param p_model: porosity profile type:
+                        1) Constant (for modeling short depths)
+                        2) Linear (typical choice for small depths)
+                        3) Exponential (typical choice for medium depths)
+                        4) Stratified (for modeling high depths)
+                        5) Noisy (assumes random "fractures" in the underground)
+        """
+
+        # Store the type of profile.
+        self.p_model = p_model
 
         # Get the size of the spatial grid.
         len_z = z_grid.size
@@ -20,14 +43,14 @@ class Porosity(object):
         (_, l_sapr, l_wbed, l_fbed) = layers
 
         # Find the indices of each underground layer.
-        self.sap_layer_idx = (z_grid >= l_sapr) & (z_grid <= l_wbed)
-        self.web_Layer_idx = (z_grid >= l_wbed) & (z_grid <= l_fbed)
+        sap_layer_idx = np.array((z_grid >= l_sapr) & (z_grid <= l_wbed), dtype=bool)
+        web_layer_idx = np.array((z_grid >= l_wbed) & (z_grid <= l_fbed), dtype=bool)
 
         # Create the profile according to the selected type.
         if str.upper(p_model) == "CONSTANT":
 
             # The porosity is set to its maximum value.
-            q_sat = theta.max * np.ones(len_z, 1)
+            q_sat = theta.max * np.ones(len_z)
 
         elif str.upper(p_model) == "LINEAR":
 
@@ -50,29 +73,35 @@ class Porosity(object):
             # Initialize the field at maximum value.
             q_sat = theta.max * np.ones(len_z)
 
-            if np.any(self.sap_layer_idx):
+            if np.any(sap_layer_idx):
                 # Fit a straight (decreasing) line in the saprolite layer.
-                q_sat[self.sap_layer_idx] = np.linspace(theta.max, theta.mid,
-                                                        self.sap_layer_idx.sum())
+                q_sat[sap_layer_idx] = np.linspace(theta.max, theta.mid, sap_layer_idx.sum())
             # _end_if_
 
-            if np.any(self.web_Layer_idx):
+            if np.any(web_layer_idx):
                 # Compute the two parameters of the exponential function:
                 # f(z) = p0*exp(-z*p1),
                 p0 = theta.mid
                 p1 = np.log(p0 / theta.min) / z_grid[-1]
 
                 # Fit the exponential function.
-                q_sat[self.web_Layer_idx] = p0 * np.exp(-np.linspace(0, l_fbed,
-                                                                     self.web_Layer_idx.sum()) * p1)
+                q_sat[web_layer_idx] = p0 * np.exp(-np.linspace(0, l_fbed, web_layer_idx.sum()) * p1)
             # _end_if_
 
         elif str.upper(p_model) == "NOISY":
 
             # Log-Normal functions.
-            logN_muf = lambda m, v: np.log(m ** 2) / np.sqrt(v + m ** 2)
-            logN_sig = lambda m, v: np.sqrt(np.log(v / (m ** 2) + 1.0))
-            logN_rnd = lambda mu, sig, en: np.exp(mu + sig * en)
+            def logN_muf(mi, vi):
+                return np.log(mi ** 2) / np.sqrt(vi + mi ** 2)
+            # _end_def_
+
+            def logN_sig(mi, vi):
+                return np.sqrt(np.log(vi / (mi ** 2) + 1.0))
+            # _end_def_
+
+            def logN_rnd(mu, sig, en):
+                return np.exp(mu + sig * en)
+            # _end_def_
 
             # Variance Scale Factor (small positive number).
             _vsf = 5.0e-5
@@ -80,25 +109,24 @@ class Porosity(object):
             # Initialize the field at maximum value.
             q_sat = theta.max * np.ones(len_z)
 
-            if np.any(self.sap_layer_idx):
+            if np.any(sap_layer_idx):
                 # Number of cells in the saprolite layer.
-                n_sap = self.sap_layer_idx.sum()
+                n_sap = sap_layer_idx.sum()
 
                 # Mean (saprolite) values.
                 mean_sap = np.linspace(theta.max, theta.mid, n_sap)
 
                 # Noise model components (mean, variance).
-                mu_sap = logN_muf(mean_sap, _vsf * np.min(mean_sap))
+                mu0_sap = logN_muf(mean_sap, _vsf * np.min(mean_sap))
                 sig_sap = logN_sig(mean_sap, _vsf * np.min(mean_sap))
 
                 # Update the profile in the sparolite zone.
-                q_sat[self.sap_layer_idx] = logN_rnd(mu_sap, sig_sap,
-                                                     np.random.randn(n_sap, 1))
+                q_sat[sap_layer_idx] = logN_rnd(mu0_sap, sig_sap, np.random.randn(n_sap))
             # _end_if_
 
-            if np.any(self.web_Layer_idx):
+            if np.any(web_layer_idx):
                 # Number of cells in the weathered bedrock layer.
-                n_web = self.web_Layer_idx.sum()
+                n_web = web_layer_idx.sum()
 
                 # Compute the two parameters of the exponential function:
                 p0 = theta.mid
@@ -108,12 +136,11 @@ class Porosity(object):
                 mean_web = p0 * np.exp(-np.linspace(0, l_fbed, n_web) * p1)
 
                 # Noise model components (mean, variance).
-                mu_web = logN_muf(mean_web, _vsf * np.min(mean_web))
+                mu1_web = logN_muf(mean_web, _vsf * np.min(mean_web))
                 sig_web = logN_sig(mean_web, _vsf * np.min(mean_web))
 
                 # Update the profile in the weathered bedrock zone.
-                q_sat[self.web_Layer_idx] = logN_rnd(mu_web, sig_web,
-                                                     np.random.randn(n_web, 1))
+                q_sat[web_layer_idx] = logN_rnd(mu1_web, sig_web, np.random.randn(n_web))
             # _end_if_
 
         else:
@@ -127,42 +154,52 @@ class Porosity(object):
         self.profile = q_sat.flatten()
 
         # Extract soil parameters.
-        n = soil.n
-        m = soil.m
-        a0 = soil.alpha
+        (n, m, a0) = (soil.n, soil.m, soil.alpha)
 
-        # helper function.
-        fun_wrc = lambda psi: theta.res + (q_sat - theta.res) / (1.0 + (a0 * psi) ** n) ** m
+        # Helper function.
+        def fun_wrc(psi):
+            return theta.res + (q_sat - theta.res) / (1.0 + (a0 * psi) ** n) ** m
+        # _end_def_
 
         # Field capacity over the whole z-domain.
         self.field_cap = np.maximum(fun_wrc(theta.flc), theta.res)
 
         # Wilting points over the whole z-domain.
         self.wilting_point = np.minimum(fun_wrc(theta.wlt), self.field_cap)
+
+        # Interpolate the profiles on the grid and store the functions.
+        self.f_interp = interp1d(z_grid, [self.profile, self.field_cap, self.wilting_point])
     # _end_def_
 
-    def __call__(self, z=None):
+    def __call__(self, z_new=None):
         """
-        TBD
+        A call of the porosity object will return a tuple containing all three
+        profiles (porosity, field capacity, wilting point) for the speicific
+        input 'z_new'. If no new depth values 'z' are given, the call will return
+        the full profiles.
 
-        :param z:
+        :param z_new: the depth(s) at which we want the new profiles.
         :return: the porosity profile at depth(s) 'z'.
         """
 
-        # Return all the profiles (over the whole domain).
-        if not z:
-            return self.profile
+        # Return all the profiles (over the whole z-domain).
+        if not z_new:
+            return self.profile, self.field_cap, self.wilting_point
+        else:
+            # Return all three profiles at the new input 'z_new'.
+            return self.f_interp(z_new)
         # _end_if_
-
-        # Get the pre-computed porosity profile (over the whole domain).
-        porosity_z = self.profile
-
-        return porosity_z
 
     # _end_def_
 
     def __str__(self):
-        pass
+        """
+        Override to print a readable string presentation of the Porosity object.
+        At the moment this will include its id(), along with its profile type.
+
+        :return: a string representation of a Porosity object.
+        """
+        return " Porosity Id({0}): Type={1}".format(id(self), self.p_model)
     # _end_def_
 
 # _end_class_
