7692ae801859f5aa77bc07037fb8e4f39c339cb6
vrettasm <vrettasm@gmail.com>
2020-06-17 14:50:14 +0200

    Updated the test with the new default_rng.

--- code/tests/test_utilities.py
+++ code/tests/test_utilities.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 from math import isclose
+from numpy.random import default_rng
 from code.src.utilities import logN_rnd
 
 class TestUtilities(unittest.TestCase):
@@ -12,11 +13,12 @@ class TestUtilities(unittest.TestCase):
         # Number of samples.
         n_samples = 25000
 
-        # Set the random seed (for reproducibility.
-        np.random.seed(0)
+        # Get a random number generator.
+        # Set the seed = 0 (for reproducibility.
+        rng = default_rng(0)
 
         # Sample n_samples parameters.
-        x = logN_rnd(mu, sigma, np.random.randn(n_samples))
+        x = logN_rnd(mu, sigma, rng.standard_normal(n_samples))
 
         # Check if the result is close enough (~ 1.0e-3).
         self.assertTrue(isclose(np.mean(x), mu, rel_tol=1.0e-3))
