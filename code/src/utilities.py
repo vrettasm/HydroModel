034bc6c92feb5e73b46316608e33bef7595da5e0
vrettasm <vrettasm@gmail.com>
2020-06-17 12:03:10 +0200

    Combined the log-N functions in one, and added documentation.

--- code/src/utilities.py
+++ code/src/utilities.py
@@ -1,42 +1,31 @@
 import numpy as np
 
-def logN_muf(mi, vi):
+def logN_rnd(mx, vx, en):
     """
-    Log-Normal functions.
+    Log-Normal function. If X follows the log-normal distribution with parameters 'mu'
+    and 'sigma': X ~ LogN(mu, sigma), then log(X) follows the normal distribution with
+    mean 'mu' and standard deviation 'sigma': log(X) ~ N(mu, sigma).
 
-    :param mi:
+    Example:
+    >> x = logN_rnd(5.0, 1.0, np.random.randn(10000))
+    >> x_mean = np.mean(x)
+    >> ... 4.992025079986667 (almost 5.0)
 
-    :param vi:
+    :param mx: desired mean (of the Normal distribution).
 
-    :return:
+    :param vx: desired variance (of the Normal distribution).
+
+    :param en: random variable N(0,1)
+
+    :return: a random variable drawn from LogNormal(mu, sigma)
     """
-    return np.log((mi ** 2) / np.sqrt(vi + mi ** 2))
-# _end_def_
 
-def logN_sig(mi, vi):
-    """
-    Log-Normal function
+    # Parameter 'mu' for the LogNormal distribution.
+    mu0 = np.log((mx ** 2) / np.sqrt(vx + mx ** 2))
 
-    :param mi:
+    # Parameter 'sigma' for the LogNormal distribution.
+    sig = np.sqrt(np.log(vx / (mx ** 2) + 1.0))
 
-    :param vi:
-
-    :return:
-    """
-    return np.sqrt(np.log(vi / (mi ** 2) + 1.0))
-# _end_def_
-
-def logN_rnd(mu, sig, en):
-    """
-    Log-Normal function
-
-    :param mu:
-
-    :param sig:
-
-    :param en:
-
-    :return:
-    """
-    return np.exp(mu + sig * en)
+    # Random variable with (mu, sigma).
+    return np.exp(mu0 + sig * en)
 # _end_def_
