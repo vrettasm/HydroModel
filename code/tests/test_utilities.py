d62a259e38579a75ed0f7cdf18714130bdb401c3
vrettasm <vrettasm@gmail.com>
2020-06-17 14:38:19 +0200

    Added test for the utilities module.

---
+++ code/tests/test_utilities.py
@@ -0,0 +1,27 @@
+import unittest
+import numpy as np
+from math import isclose
+from code.src.utilities import logN_rnd
+
+class MyTestCase(unittest.TestCase):
+
+    def test_logN_rnd(self):
+        # Log-Normal parameters.
+        mu, sigma = 20.5, 1.0
+
+        # Number of samples.
+        n_samples = 25000
+
+        # Set the random seed (for reproducibility.
+        np.random.seed(0)
+
+        # Sample n_samples parameters.
+        x = logN_rnd(mu, sigma, np.random.randn(n_samples))
+
+        # Check if the result is close enough (~ 1.0e-3).
+        self.assertTrue(isclose(np.mean(x), mu, rel_tol=1.0e-3))
+    # _end_def_
+
+
+if __name__ == '__main__':
+    unittest.main()
