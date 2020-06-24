57ca6fff0374d2aa241d435c5043e2923403546e
vrettasm <vrettasm@gmail.com>
2020-06-24 19:23:28 +0200

    Added the find_wtd function in the utilities module.

--- code/src/utilities.py
+++ code/src/utilities.py
@@ -34,3 +34,44 @@ def logN_rnd(mx, vx, en):
     # Random variable with (mu, sigma).
     return np.exp(mu0 + sig * en)
 # _end_def_
+
+def find_wtd(y=None):
+    """
+    This function identifies  the location of  the water table  depth (wtd)
+    and returns  its index value.  It starts the search  from the bottom of
+    the input vector and continues going upwards until the cell in question
+    is not saturated. It assumes that the "wtd" exists right below the first
+    non-saturated cell.
+
+    :param y: bool vector with the saturated cells. [dim_d x 1].
+
+    :return: the index at the first "True", from the end of the vector.
+    """
+
+    # If no input, return None.
+    if y is None:
+        return None
+    # _end_if_
+
+    # Get the length of the input vector.
+    dim_d = y.size
+
+    # Initialize return index. In the unlikely event that the whole state
+    # vector is saturated, the wtd should point at the surface (1st cell).
+    i = 0
+
+    # Initialize the index at the bottom.
+    #  - i.e. start the search backwards.
+    for j, y_n in enumerate(reversed(y)):
+        # Check if the cell is saturated.
+        if not y_n:
+            i = dim_d - j
+            break
+        # _end_if_
+    # _end_for_
+
+    # Make sure the index does not exceed the upper limit: return index "i"
+    # should be in [0, dim_d-1].  This is because  we assume that the |wtd|
+    # always exists inside the spatial domain.
+    return np.minimum(i, dim_d-1)
+# _end_def_
