9653fff49d379d523b0cde2679af3d9cbfae54ce
vrettasm <vrettasm@gmail.com>
2020-06-26 13:37:35 +0200

    Updated the test to reflect the changes in the TreeRoots class.

--- code/tests/test_tree_roots.py
+++ code/tests/test_tree_roots.py
@@ -1,10 +1,13 @@
 import unittest
-import numpy as np
 from math import isclose
+
+import numpy as np
+
 from code.src.porosity import Porosity
+from code.src.soil_properties import SoilProperties
 from code.src.tree_roots import TreeRoots
 from code.src.water_content import WaterContent
-from code.src.soil_properties import SoilProperties
+
 
 class TestTreeRoots(unittest.TestCase):
     @classmethod
@@ -72,7 +75,7 @@ class TestTreeRoots(unittest.TestCase):
             root_pdf_1 = test_obj()
 
             # Check if the integrated density sums to one.
-            self.assertTrue(isclose(np.sum(root_pdf_1, axis=0) * self.dz, 1.0, rel_tol=1.0e-5))
+            self.assertTrue(isclose(np.sum(root_pdf_1) * self.dz, 1.0, rel_tol=1.0e-5))
 
             # Make sure the max root depths match.
             self.assertEqual(test_obj.max_root_depth, (self.ln * self.dz))
@@ -81,8 +84,8 @@ class TestTreeRoots(unittest.TestCase):
             root_pdf_2 = test_obj(self.z_roots)
 
             # Check if the densities are almost equal.
-            self.assertTrue(isclose(np.sum(root_pdf_1, axis=0) * self.dz,
-                                    np.sum(root_pdf_2, axis=0) * self.dz,
+            self.assertTrue(isclose(np.sum(root_pdf_1) * self.dz,
+                                    np.sum(root_pdf_2) * self.dz,
                                     rel_tol=1.0e-4))
         # _end_for_
 
@@ -101,24 +104,24 @@ class TestTreeRoots(unittest.TestCase):
         self.assertTrue(np.sum(rho_theta0) == 0.0)
 
         # Simple version.
-        theta_1d = np.random.rand(self.ln, 1)
+        theta_1d = np.random.rand(self.ln)
         rho_theta1, _ = test_obj.efficiency(theta_1d, self.z_roots)
 
         # Check the input/output dimensions.
         self.assertEqual(theta_1d.shape, rho_theta1.shape)
 
         # Check if the integrated water efficiency sums to one.
-        self.assertTrue(isclose(np.sum(rho_theta1, axis=0) * self.dz, 1.0, rel_tol=1.0e-5))
+        self.assertTrue(isclose(np.sum(rho_theta1) * self.dz, 1.0, rel_tol=1.0e-5))
 
         # Vectorized version.
-        theta_2d = np.random.rand(self.ln, 4)
+        theta_2d = np.random.rand(4, self.ln)
         rho_theta2, _ = test_obj.efficiency(theta_2d, self.z_roots)
 
         # Check the input/output dimensions.
         self.assertEqual(theta_2d.shape, rho_theta2.shape)
 
         # Check if the integrated water efficiency sums to one.
-        total_sum = np.sum(rho_theta2, axis=0) * self.dz
+        total_sum = np.sum(rho_theta2, axis=1) * self.dz
         for sum_i in total_sum:
             # Check each dimension in the vector.
             self.assertTrue(isclose(sum_i, 1.0, rel_tol=1.0e-5))
