12b3aa4cd2627491466ad21aa6b30e771218c6ba
vrettasm <vrettasm@gmail.com>
2020-07-03 16:53:49 +0200

    Minor update/edit.

--- code/src/simulation.py
+++ code/src/simulation.py
@@ -368,7 +368,7 @@ class Simulation(object):
 
                 # Check if the dimensions match.
                 if z_grid.shape != init_cond.shape:
-                    raise RuntimeError(" {0} : Initial conditions vector. The dimensions"
+                    raise RuntimeError(" {0} : Initial conditions vector's dimensions"
                                        " do not match the spatial grid.".format(self.__class__.__name__))
                 # _end_if_
             # _end_with_
