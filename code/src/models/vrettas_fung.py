749d55c5915f1427fb852c90b77925b3253e51a4
vrettasm <vrettasm@gmail.com>
2020-07-09 22:16:16 +0200

    Minor edit in documentation.

--- code/src/models/vrettas_fung.py
+++ code/src/models/vrettas_fung.py
@@ -10,13 +10,13 @@ class VrettasFung(HydrologicalModel):
     This class represents a hydrologic model based on the Vrettas-Fung papers.
 
     1) Vrettas, M. D., and I. Y. Fung (2015), "Toward a new parameterization of
-    hydraulic conductivity in climate models: Simulation of rapid groundwater
+    hydraulic  conductivity in climate models:  Simulation of rapid groundwater
     fluctuations in Northern California", Journal of Advances in Modeling Earth
     Systems, 07, doi:10.1002/2015MS000516.
 
     2) Vrettas, M. D., and I. Y. Fung (2017), "Sensitivity of transpiration to
-    subsurface properties: Exploration with a 1-D model", Journal of Advances in
-    Modelling Earth Systems, 9, doi:10.1002/2016MS000901.
+    subsurface properties:  Exploration with a 1-D model", Journal of Advances
+    in Modeling Earth Systems, 9, doi:10.1002/2016MS000901.
     """
 
     def __init__(self, soil, porous, k_hc, theta_res, dz):
